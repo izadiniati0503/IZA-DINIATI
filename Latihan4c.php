32a22dd77b962162ff8ee645e4f67247986cca55
izadiniati0503 <114216724+izadiniati0503@users.noreply.github.com>
2022-09-23 15:45:56 +0700

    Create Latihan4c.php

---
+++ Latihan4c.php
@@ -0,0 +1,29 @@
+<?php
+$negara = [
+                "Indonesia" => "D.K.I Jakarta",
+                "Singapura" => "Singapura",
+                "Malaysia" => "Kuala Lumpur",
+                "Brunei" => "Bandar Seri Begawan",
+                "Thailand" => "Bangkok",
+                "Laos" => "Vientiane",
+                "Filipina" => "Manila",
+                "Myanmar" => "Naupyidaw"];
+                // untuk memasukan value, kita harus membuat key yang merepresentasikan valuenya.
+                // tanda "=>" dapat diartikan sebagai merujuk
+                //"key" => "value"
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Associative</title>
+    </head>
+    <body>
+        <p>
+            <b>Daftar Negara ASEAN dan Ibukota : <br></b>
+        <?php foreach ($negara as $kota => $ibukota) : ?>
+            <li><?php echo "$kota : $ibukota" ?></li>
+        <?php endforeach ?>
+        </p>
+</body>
+</html>
